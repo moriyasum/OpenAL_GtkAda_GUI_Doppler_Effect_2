0778c8d6657e79a544e0a4223bb250d51913c448
Moriys <moriyasu.motohara@gmail.com>
2025-08-29 17:35:29 +0900

    Add files via upload

--- src/gui.ads
+++ src/gui.ads
@@ -2,42 +2,42 @@
 with Gtk.Box;         use Gtk.Box;
 with Gtk.Label;       use Gtk.Label;
 with Gtk.Widget;      use Gtk.Widget;
-with Gtk.Main;        use Gtk.Main;
-with Gtk.Window;      use Gtk.Window;
+--  with Gtk.Main;        use Gtk.Main;
+--  with Gtk.Window;      use Gtk.Window;
 with Gtk.Frame;       use Gtk.Frame;
 with Gtk.Button;          use Gtk.Button;
 with Gtk.Enums;           use Gtk.Enums;
 with Gtk.Handlers;        use Gtk.Handlers;
-with Gtk.Scrolled_Window; use Gtk.Scrolled_Window;
-with Gtk.Adjustment;      use Gtk.Adjustment;
+--  with Gtk.Scrolled_Window; use Gtk.Scrolled_Window;
+--  with Gtk.Adjustment;      use Gtk.Adjustment;
 with Gtk.Drawing_Area;    use Gtk.Drawing_Area;
 with Gtk.Spin_Button;     use Gtk.Spin_Button;
 with Gtkada.Canvas;       use Gtkada.Canvas;
-with Gtkada.Canvas_View;  use Gtkada.Canvas_View;
-with Gtkada.Canvas_View.Views; use Gtkada.Canvas_View.Views;
-with Gtkada.Style;        use Gtkada.Style;
+--  with Gtkada.Canvas_View;  use Gtkada.Canvas_View;
+--  with Gtkada.Canvas_View.Views; use Gtkada.Canvas_View.Views;
+--  with Gtkada.Style;        use Gtkada.Style;
 
 with Ada.Numerics.Discrete_Random;
-with Ada.Numerics.Elementary_Functions;    --For Sin
-use  Ada.Numerics.Elementary_Functions;     --For Sin
+--  with Ada.Numerics.Elementary_Functions;    --  For Sin
+--  use  Ada.Numerics.Elementary_Functions;     --  For Sin
 with Ada.Numerics;      use Ada.Numerics;
-with Ada.Float_Text_IO; use Ada.Float_Text_IO;
+--  with Ada.Float_Text_IO; use Ada.Float_Text_IO;
 
 with Gdk;               use Gdk;
-with Gdk.Cairo;         use Gdk.Cairo;
+--  with Gdk.Cairo;         use Gdk.Cairo;
 with Gdk.RGBA;          use Gdk.RGBA;
 with Gdk.Pixbuf;        use Gdk.Pixbuf;
 with Glib;              use Glib;
-with Glib.Error;        use Glib.Error;
+--  with Glib.Error;        use Glib.Error;
 with Glib.Object;       use Glib.Object;
 with Cairo;             use Cairo;
-with Cairo.Pattern;     use Cairo.Pattern;
-with Cairo.Png;         use Cairo.Png;
-with Cairo.Region;      use Cairo.Region;
-with Cairo.Surface;     use Cairo.Surface;
-with OpenAL.Context;    use OpenAL.Context;
-with Text_IO;           use Text_IO;
-
+--  with Cairo.Pattern;     use Cairo.Pattern;
+--  with Cairo.Png;         use Cairo.Png;
+--  with Cairo.Region;      use Cairo.Region;
+--  with Cairo.Surface;     use Cairo.Surface;
+--  with OpenAL.Context;    use OpenAL.Context;
+--  with Ada.Text_IO;           use Ada.Text_IO;
+--
 
 package Gui is
 
@@ -51,32 +51,32 @@ package Gui is
    subtype Coordinate_Type is Gint range Default_Grid_Size + 1 .. Max_Size;
    package Coordinate_Random is new
       Ada.Numerics.Discrete_Random (Coordinate_Type);
-   use Coordinate_Random;
+--   use Coordinate_Random;
 
    subtype Zoom_Type is Gint range 1 .. 2;
    package Zoom_Random is new Ada.Numerics.Discrete_Random (Zoom_Type);
-   use Zoom_Random;
+--   use Zoom_Random;
 
-   type FixedPoint0018 is delta 0.001 digits 8;   --To display Float with FixedPoint value
+   type FixedPoint0018 is delta 0.001 digits 8;
    FixPnt008 : FixedPoint0018;
-   type FixedPoint0016 is delta 0.1 digits 6;   --To display Float with FixedPoint value
+   type FixedPoint0016 is delta 0.1 digits 6;
    FixPnt006 : FixedPoint0016;
 
 ----------------------------------------------------------------
 --  Redefine our own item type, since we want to provide our own
 --  graphics.
 ----------------------------------------------------------------
-   type Display_Item_Record is new GtkAda.Canvas.Canvas_Item_Record with record
+   type Display_Item_Record is new Gtkada.Canvas.Canvas_Item_Record with record
       Canvas : Interactive_Canvas;
       Color  : Gdk.RGBA.Gdk_RGBA;
       W, H   : Gint;
       Num    : Positive;
    end record;
    type Display_Item is access all Display_Item_Record'Class;
-
-
+--
+--
 ----------------------------------------------------
--- Our own canvas, with optional background image --
+--  Our own canvas, with optional background image --
 ----------------------------------------------------
    type Image_Canvas_Record is new Interactive_Canvas_Record with record
       Background : Cairo_Pattern := Null_Pattern;
@@ -97,108 +97,112 @@ package Gui is
      (Interactive_Canvas_Record);
    package Canvas_User_Cb is new Gtk.Handlers.User_Callback
      (Gtk_Widget_Record, Image_Canvas);
-
-
+--
+--
 ------------------------
 -- Callbacks packages --
 ------------------------
-package Expose_Cb is new Gtk.Handlers.Return_Callback
-  (Image_Drawing_Record, Boolean);
+   package Expose_Cb is new Gtk.Handlers.Return_Callback
+     (Image_Drawing_Record, Boolean);
 
-package Destroy_Cb is new Gtk.Handlers.Callback (Image_Drawing_Record);
-
-
+   package Destroy_Cb is new Gtk.Handlers.Callback (Image_Drawing_Record);
+--
+--
 -----------------------
--- PROTYPE
+--  PROTYPE
 -----------------------
 
-procedure Run (Frame : access Gtk.Frame.Gtk_Frame_Record'Class);
+   procedure Run (Frame : access Gtk.Frame.Gtk_Frame_Record'Class);
 
-procedure Button_Single_Cb
+   procedure Button_Single_Cb
      (Canvas : access Interactive_Canvas_Record'Class);
-procedure Button_Loop_Cb
+   procedure Button_Loop_Cb
      (Canvas : access Interactive_Canvas_Record'Class);
-procedure Button_Step_Cb
+   procedure Button_Step_Cb
      (Canvas : access Interactive_Canvas_Record'Class);
-procedure Button_Clear_Cb
+   procedure Button_Clear_Cb
      (Canvas : access Interactive_Canvas_Record'Class);
 
-procedure Draw       --Draw #1 *********
+   procedure Set_Button_Style
+     (Button : Gtk_Button; Color : String); --  Added 8/25/2025
+
+   overriding
+   procedure Draw       --  Draw #1 *********
      (Item   : access Display_Item_Record;
       Cr     : Cairo_Context);
 
-procedure Initial_Plane_Item_Setup
+   procedure Initial_Plane_Item_Setup
      (Canvas : access Interactive_Canvas_Record'Class);
 
-procedure Initialize_Listener_Image
+   procedure Initialize_Listener_Image
      (Draw   : out Image_Drawing;
       Pixbuf : Gdk.Pixbuf.Gdk_Pixbuf;
       Title  : String);
 
-function On_Draw_Listener_Image
+   function On_Draw_Listener_Image
       (Draw : access Image_Drawing_Record'Class;
        Cr   : Cairo_Context) return Boolean;
 
- Procedure Calculate_Display_Sound
-  (Counter : in Integer);
+   procedure Calculate_Display_Sound
+     (Counter : Integer);
 
-Procedure Display_Plane
-   (Counter : in Integer);
+   procedure Display_Plane
+     (Counter : Integer);
 
-procedure On_Listner_Loc (Spin : access Glib.Object.GObject_Record'Class);
+   procedure On_Listner_Loc (Spin : access Glib.Object.GObject_Record'Class);
 
-
+--
 -----------------------------
--- GLOBAL VARIABLES
+--  GLOBAL VARIABLES
 -----------------------------
-Button_Single       : Gtk_Button;
-Button_Loop         : Gtk_Button;
-Button_Step         : Gtk_Button;
-Button_Clear        : Gtk_Button;
-ButtonColor         : Gdk_RGBA;
-Dummy_Boolean       : Boolean;
+   Button_Single       : Gtk_Button;
+   Button_Loop         : Gtk_Button;
+   Button_Step         : Gtk_Button;
+   Button_Clear        : Gtk_Button;
+   ButtonColor         : Gdk_RGBA;
+   Dummy_Boolean       : Boolean;
 
-Label_Dummy1  : Gtk_Label;   --Left pane top for clearance
-Label_TCnt    : Gtk_Label;   --Left pane,  TimeCnt
-Label_S2SndPos_X : Gtk_Label;   --Left pane,  New position X
-Label_S2SndPos_Y : Gtk_Label;   --Left pane,  New position Y
-Label_SndVel_X :   Gtk_Label;   --Left pane,  Sound Veloctiy X
-Label_SndVel_Y :   Gtk_Label;   --Left pane,  Sound Veloctiy Y
-Label_Speed_Angle : Gtk_Label;     --Left pane,  Sound Angle
-Label_Start_Flag :  Gtk_Label;     --Left pane,  Status
+   Label_Dummy1  : Gtk_Label;   --  Left pane top for clearance
+   Label_TCnt    : Gtk_Label;   --  Left pane,  TimeCnt
+   Label_S2SndPos_X : Gtk_Label;   --  Left pane,  New position X
+   Label_S2SndPos_Y : Gtk_Label;   --  Left pane,  New position Y
+   Label_SndVel_X :   Gtk_Label;   --  Left pane,  Sound Veloctiy X
+   Label_SndVel_Y :   Gtk_Label;   --  Left pane,  Sound Veloctiy Y
+   Label_Speed_Angle : Gtk_Label;     --  Left pane,  Sound Angle
+   Label_Start_Flag :  Gtk_Label;     --  Left pane,  Status
 
-OFFSET_X      : Gtk_Spin_Button;
-OFFSET_Y      : Gtk_Spin_Button;
-PIXFACTOR_X   : Gtk_Spin_Button;
-PIXFACTOR_Y   : Gtk_Spin_Button;
-SND_POS_FACTOR : Gtk_Spin_Button;   --Sound Distance Factor
-SND_SPD_FACTOR : Gtk_Spin_Button;   --Sound SPeed Factor
-TIME_CYCLE_X  : Gtk_Spin_Button;
-TIME_CYCLE_Y  : Gtk_Spin_Button;
-LPOS_X        : Gtk_Spin_Button;
-LPOS_Y        : Gtk_Spin_Button;
-Buf_LPOS_X    : float;
-Buf_LPOS_Y    : float;
+   OFFSET_X      : Gtk_Spin_Button;
+   OFFSET_Y      : Gtk_Spin_Button;
+   PIXFACTOR_X   : Gtk_Spin_Button;
+   PIXFACTOR_Y   : Gtk_Spin_Button;
+   SND_POS_FACTOR : Gtk_Spin_Button;   --  Sound Distance Factor
+   SND_SPD_FACTOR : Gtk_Spin_Button;   --  Sound SPeed Factor
+   TIME_CYCLE_X  : Gtk_Spin_Button;
+   TIME_CYCLE_Y  : Gtk_Spin_Button;
+   LPOS_X        : Gtk_Spin_Button;
+   LPOS_Y        : Gtk_Spin_Button;
+   Buf_LPOS_X    : Float;
+   Buf_LPOS_Y    : Float;
 
-S1Pos_X  : float := 99999.9;    -- Source X Coordinate OLD Memory, @previous Timer Interrupt Position
-S1Pos_Y  : float := 99999.9;    -- Source Y Coordinate OLD Memory, @previous Timer Interrupt Position
-S2Pos_X  : float := 99999.9;    -- Source X Coordinate NEW, Normalized value
-S2Pos_Y  : float := 99999.9;    -- Source Y Coordinate NEW, Normalized value
-S2PixPos_X  : float;   --Source Display Pixel Absolute Position, = S2Pos_X * PIXFACTOR_X
-S2PixPos_Y  : float;   --Source Display Pixel Absolute Position, = S2Pos_Y * PIXFACTOR_Y
-S2SndPos_X  : float;   --Sound Relative Positon from Listener, = (S2Pos_X - Buf_LPOS_X) * SND_POS_FACTOR
-S2SndPos_Y  : float;   --Sound Relative Positon from Listener, = (S2Pos_Y - Buf_LPOS_Y) * SND_POS_FACTOR
-Distance_SL : float;   --Distance Source-Listener * Sound_Factor
-Vel_Snd_X : float;         --Plane Velocity for Sound, SND_SPD_FACTOR * (X2-X1)/Intr_cycle
-Vel_Snd_Y : float;         --Plane Velocity for Sound, SND_SPD_FACTOR * (Y2-Y1)/Intr_cycle
-Speed_Angle : float :=0.0;  --Plane Angle [rad]
-Speed_NOM : float;          --Plane Speed Nominal Scalar
-Speed_Pix  : float;         --Plane Speed Pixel Scalar
-Time_X, Time_Y  : Float;    --Figure Tracing Timer counter
-Draw_Listener_Counter : Integer :=0;   --Counter how many times Draw_Listener called by System
-Draw_Plane_Counter : Integer := 0;     --Counter how many times Draw_Plane called by System
-Step_Counter : Integer := 0;   --Set STEP mode, how many Interrupts, downcounter, preset=STEP_NUMBER.
-STEP_NUMBER  : constant := 3;  --How many steps for "Step". It presets Step_Counter
-PLANE_BODY_COLOR : constant Gdk.RGBA.Gdk_RGBA := (0.0, 0.0, 0.8, 1.0); --RGBA (Blue)
+   S1Pos_X  : Float := 99999.9;  --  @previous Timer Interrupt Position
+   S1Pos_Y  : Float := 99999.9;  --  @previous Timer Interrupt Position
+   S2Pos_X  : Float := 99999.9;  --  Source X Coordinate NEW, Normalized value
+   S2Pos_Y  : Float := 99999.9;  --  Source Y Coordinate NEW, Normalized value
+   S2PixPos_X  : Float;  --  Source Absolute Position, = S2Pos_X * PIXFACTOR_X
+   S2PixPos_Y  : Float;  --  Source Absolute Position, = S2Pos_Y * PIXFACTOR_Y
+   S2SndPos_X  : Float;   --  (S2Pos_X - Buf_LPOS_X) * SND_POS_FACTOR
+   S2SndPos_Y  : Float;   --  (S2Pos_Y - Buf_LPOS_Y) * SND_POS_FACTOR
+   Distance_SL : Float;   --  Distance Source-Listener * Sound_Factor
+   Vel_Snd_X : Float;     --  SND_SPD_FACTOR * (X2-X1)/Intr_cycle
+   Vel_Snd_Y : Float;     --  SND_SPD_FACTOR * (Y2-Y1)/Intr_cycle
+   Speed_Angle : Float := 0.0;  --  Plane Angle [rad]
+   Speed_NOM : Float;          --  Plane Speed Nominal Scalar
+   Speed_Pix  : Float;         --  Plane Speed Pixel Scalar
+   Time_X, Time_Y  : Float;    --  Figure Tracing Timer counter
+   Draw_Listener_Counter : Integer := 0; --  Cnter Draw_Listener called by Sys
+   Draw_Plane_Counter : Integer := 0;   --  Counter Draw_Plane called by System
+   Step_Counter : Integer := 0; --  Set STEP mode, Counter Interrupts, downCnt
+   STEP_NUMBER  : constant := 3;  --  For "Step". It presets Step_Counter
+   PLANE_BODY_COLOR : constant Gdk.RGBA.Gdk_RGBA := (0.0, 0.0, 0.8, 1.0);
 
 end Gui;
--- src/tintr.adb
+++ src/tintr.adb
@@ -1,95 +1,108 @@
-
-
+--
+--
 with Gui;          use Gui;
 with Audio;        use Audio;
+with Ada.Text_IO;        use Ada.Text_IO;
+with Gtk.Button;         use Gtk.Button;
+with Gtk.Spin_Button;    use Gtk.Spin_Button;
+with Gdk.RGBA;           use Gdk.RGBA;
+with OpenAL.Context;
+with OpenAL.Source;
 
-Package body Tintr is
+package body Tintr is
 --------------------------------------------------------
 --------------------------------------------------------
---TIMER INTERRUPT
+--  TIMER INTERRUPT
 --------------------------------------------------------
 --------------------------------------------------------
-function Timer_Intr (Pbar : Gtk_Progress_Bar) return Boolean is
-
-begin
-   If(Start_Flag =0) then    --Flag=0  Nothing to do
-      TimerIntrCounter := TimerIntrCounter + 1;
-      TimeCnt := 0;
-      Time_X  := 0.0;
-      Time_Y  := 0.0;
-      Return True;
-   end if;
+   function Timer_Intr (Pbar : Gtk_Progress_Bar) return Boolean is
+      pragma Unreferenced (Pbar);
+   begin
+      if Start_Flag = 0 then    --  Flag=0  Nothing to do
+         TimerIntrCounter := TimerIntrCounter + 1;
+         TimeCnt := 0;
+         Time_X  := 0.0;
+         Time_Y  := 0.0;
+         return True;
+      end if;
 
 --------------------
--- Display Graphics and Audio Output
---------------------     
-   Display_Plane (TimeCnt);       --Calculate and Move Plane
-   ALSound_Process;    --Set Audio Source & Listner Position XYZ and Source Velocity XYZ
-         
-   if (Pause_Flag=1) then   --Pause, then do nothing (keep audio and position)
-      TimerIntrCounter := TimerIntrCounter + 1;
-      Return True;
-   end if;      
-
-   if ((Start_Flag=1) or (Start_Flag=3) or (Start_Flag=5)) then
-      --Button was pressed, Begin Audio
-      OpenAL.Source.Set_Looping(Audio.Sound_Source, Looping => True);   --Manual doc is wrong 
-      OpenAL.Source.Play (Audio.Sound_Source_Array(1));
-
-      Put_Line("Play(Sound_Source)");
-
-      If ((Start_Flag=1) or (Start_Flag=5)) then
-         TimeCnt := 0;    --Initialize         
+--  Display Graphics and Audio Output
+--------------------
+      Display_Plane (TimeCnt);       --  Calculate and Move Plane
+--  Set Audio Source & Listner Position XYZ and Source Velocity XYZ
+      ALSound_Process;
+      if Pause_Flag = 1 then
+      --  Pause, then do nothing (keep audio and position)
+         TimerIntrCounter := TimerIntrCounter + 1;
+         return True;
       end if;
-      Start_Flag := Start_Flag + 1;   --Flag 1=>2, 3=>4, 5=>6
-   end if;
-         
---STEP mode and Counter is busy
-   If (Start_Flag=4) then   --STEP mode   
-      If (Step_Counter /= 0) then            
-         Step_Counter := Step_Counter - 1;
-         If (Step_Counter = 0) then 
-            Pause_Flag := 1;     
-            Set_Label (Button_Step, "Step");   --Turn to stop STEP             
-            Parse (ButtonColor, "Black", Dummy_Boolean); 	         
-            Gtk.Widget.Override_Color(Gtk_Widget(Button_Step), Gtk_State_Flag_Normal ,ButtonColor);   --Button Character Color is switched to Red, Modify is obsolete
+
+      if Start_Flag = 1 or else Start_Flag = 3 or else Start_Flag = 5 then
+   --  Button was pressed, Begin Audio
+   --  Manual doc is wrong
+         OpenAL.Source.Set_Looping (Audio.Sound_Source, Looping => True);
+         OpenAL.Source.Play (Audio.Sound_Source_Array (1));
+
+         Put_Line ("Play (Sound_Source)");
+
+         if Start_Flag = 1 or else Start_Flag = 5 then
+            TimeCnt := 0;    --  Initialize
+         end if;
+         Start_Flag := Start_Flag + 1;   --  Flag 1=>2, 3=>4, 5=>6
+      end if;
+
+--  STEP mode and Counter is busy
+      if Start_Flag = 4 then   --  STEP mode
+         if Step_Counter /= 0 then
+            Step_Counter := Step_Counter - 1;
+            if Step_Counter = 0 then
+               Pause_Flag := 1;
+               Set_Label (Button_Step, "Step");   --  Turn to stop STEP
+               Gui.Set_Button_Style (Button_Step, "Red");
+
             end if;
-      end if;   
-   end if; --End Start_Flag=1,3,5   
+         end if;
+      end if; --  End Start_Flag=1,3,5
 
-   TimerIntrCounter := TimerIntrCounter +1;
-   TimeCnt := TimeCnt + 1;
-   Time_X := Time_X +1.0; 
-   If ((Time_X * float(TINTR_PITCH) /1000.0) >= float(Get_Value(TIME_CYCLE_X))) then
---Timer(X) is full, Plane is at the start position, then one loop was finished. 
-      Time_X := 0.0;
-      If (Start_Flag=2) then   --Single loop complted, then stop the process.
-   --Single mode and End cycle. Finish Single process
-         Start_Flag := 0;
-         Pause_Flag := 0;
-         Set_Label (Gui.Button_Single, "Single");
-         Parse (ButtonColor, "Black", Dummy_Boolean); 	         
-         Gtk.Widget.Override_Color(Gtk_Widget(Button_Single), Gtk_State_Flag_Normal ,ButtonColor);   --Button Character Color is switched to Red, Modify is obsolete
-         Set_Label (Gui.Button_Loop, "Loop");
-         Parse (ButtonColor, "Black", Dummy_Boolean); 	         
-         Gtk.Widget.Override_Color(Gtk_Widget(Button_Loop), Gtk_State_Flag_Normal ,ButtonColor);   --Button Character Color is switched to Red, Modify is obsolete
-         Set_Label (Button_Step, "Step");
-         Parse (ButtonColor, "Black", Dummy_Boolean); 	         
-         Gtk.Widget.Override_Color(Gtk_Widget(Button_Step), Gtk_State_Flag_Normal ,ButtonColor);   --Button Character Color is switched to Red, Modify is obsolete
-                 
-         OpenAL.Context.Close_Device (Audio.CX_Devicet);  -- CLOSE "OpenAL Soft" and stop sound
-         Return True;
+      TimerIntrCounter := TimerIntrCounter + 1;
+      TimeCnt := TimeCnt + 1;
+      Time_X := Time_X + 1.0;
+      if Time_X * Float (TINTR_PITCH) / 1000.0 >=
+        Float (Get_Value (TIME_CYCLE_X))
+      then
+--  Timer(X) is full, Plane is the start position, then one loop was finished.
+         Time_X := 0.0;
+         if Start_Flag = 2 then --  Single loop end, then stop the process
+   --  Single mode and End cycle. Finish Single process
+            Start_Flag := 0;
+            Pause_Flag := 0;
+            Set_Label (Gui.Button_Single, "Single");
+            Parse (ButtonColor, "Black", Dummy_Boolean);
+            Gui.Set_Button_Style (Button_Single, "Black");
+
+            Set_Label (Gui.Button_Loop, "Loop");
+            Parse (ButtonColor, "Black", Dummy_Boolean);
+            Gui.Set_Button_Style (Button_Loop, "Black");
+
+            Set_Label (Button_Step, "Step");
+            Parse (ButtonColor, "Black", Dummy_Boolean);
+            Gui.Set_Button_Style (Button_Step, "Black");
+
+            --  CLOSE "OpenAL Soft" and stop sound
+            OpenAL.Context.Close_Device (Audio.CX_Devicet);
+            return True;
+         end if;
+--  On the way processing
       end if;
---On the way processing         
-   end if;
-   Time_Y := Time_Y +1.0;
-   If ((Time_Y * float(TINTR_PITCH) /1000.0) >= float(Get_Value(TIME_CYCLE_Y))) then
-      Time_Y := 0.0;
-   end if;
- 
-   Return True;
-end Timer_Intr;
+      Time_Y := Time_Y + 1.0;
+      if Time_Y * Float (TINTR_PITCH) / 1000.0 >=
+        Float (Get_Value (TIME_CYCLE_Y))
+      then
+         Time_Y := 0.0;
+      end if;
 
-End Tintr;
+      return True;
+   end Timer_Intr;
 
-
+end Tintr;
